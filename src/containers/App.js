fc21c70649a0b067c47ed593bb0317cbf80917fe
Siddharth Pal <originalsidd@gmail.com>
2021-05-20 00:34:38 +0530

    Added hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,56 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-// import { robots } from './robots';
-// destructured robots as its not 'export default' in robots.js
-// therefor if only export is used, it means multiple exports are present although in robots its a single element (list)
-// so we need to destructure it
+// import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+function App() {
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
+
+	useEffect(() => {
+		 fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
+			.then(users => setRobots(users));
+		}, [])
+		
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value)
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
-
-	render() {
-		const {robots, searchfield} = this.state;
-		const filteredRobots =robots.filter(robot =>  robot.name.toLowerCase().includes(searchfield.toLowerCase()));
-		return !robots.length ?
-			<h1>Loading...</h1> : (filteredRobots.length ? 
+	const filteredRobots =robots.filter(robot =>  robot.name.toLowerCase().includes(searchfield.toLowerCase()));
+	return !robots.length ?
+	<h1>Loading...</h1> : 
+	(
+		<div className='tc'>
+			<h1 className='f1'>RoboFriends</h1>
+			<SearchBox searchChange={onSearchChange} />
+			{
+				filteredRobots.length ? 
 				(
-					<div className='tc'>
-						<h1 className='f1'>RoboFriends</h1>
-						<SearchBox searchChange={this.onSearchChange} />
-						<Scroll>
-							<CardList robots={filteredRobots} />
-						</Scroll>
-					</div>
-				) : 
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				) :
 				(
-					<div className='tc'>
-						<h1 className='f1'>RoboFriends</h1>
-						<SearchBox searchChange={this.onSearchChange} />
-						<h2 className="empty">No robots with name {searchfield}</h2>
-					</div>
+					<h2 className="empty">No robots with name {searchfield}</h2>
 				)
-			)
-	}
+			}
+		</div>
+	)
 }
 
 export default App;
